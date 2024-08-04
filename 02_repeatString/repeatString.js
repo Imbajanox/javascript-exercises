649d4ed1d73b90ec0d80166963442b2328fa603f
Imbajanox <33042034+Imbajanox@users.noreply.github.com>
2024-08-04 19:29:06 +0200

    Complete exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(stringName, number) {
+    let returnString = "";
+    if (number < 0){
+        return "ERROR";
+    }
+    for (let index = 0; index < number; index++) {
+        returnString += stringName;
+    }
+    return returnString;
 };
 
 // Do not edit below this line
