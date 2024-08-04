e5dcbfb1a66fea1d8e5d5c77b77434298cbd89d4
Imbajanox <33042034+Imbajanox@users.noreply.github.com>
2024-08-04 19:37:32 +0200

    Complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(normalString) {
+    let reverseString = "";
+    for (let index = normalString.length-1; index >= 0; index--) {
+        reverseString += normalString[index]
+    }
+    return reverseString;
 };
 
 // Do not edit below this line
