dab639150edc1c39500683cb35bfaa13ec5cc66e
Imbajanox <33042034+Imbajanox@users.noreply.github.com>
2024-08-05 19:45:50 +0200

    Complete exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperatureFahrenheit) {
+  const temperatureCelsius = (temperatureFahrenheit - 32) * (5/9);
+  return Math.round(temperatureCelsius * 10)/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperatureCelsius) {
+  const temperatureFahrenheit = (temperatureCelsius  * (9/5) + 32);
+  return Math.round(temperatureFahrenheit * 10)/10;
 };
 
 // Do not edit below this line
